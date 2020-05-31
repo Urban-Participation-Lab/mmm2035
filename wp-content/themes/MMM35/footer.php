c5a0566b641e7578fbf6ff3d59f7166687e8e516
Benjamin Bädorf <hello@benjaminbaedorf.eu>
2020-05-31 19:52:47 +0200

    Updated post style

--- wp-content/themes/MMM35/footer.php
+++ wp-content/themes/MMM35/footer.php
@@ -11,12 +11,12 @@ if ( ! defined( 'ABSPATH' ) ) {
     <footer class="mmm35-footer">
       <p>Created by Urban Participation Lab</p>
 
-      <?php if ( has_nav_menu( 'primary' ) ) { ?>
+      <?php if ( has_nav_menu( 'footer' ) ) { ?>
         <?php wp_nav_menu(
           array(
             'container'  => '',
             'items_wrap' => '<ul class="mmm35-footer__nav">%3$s</ul>',
-            'theme_location' => 'primary',
+            'theme_location' => 'footer',
             'depth' => 1,
             'walker' => new MMM35WalkerNavMenu('mmm35-footer__nav'),
           )
--- wp-content/themes/MMM35/singular.php
+++ wp-content/themes/MMM35/singular.php
@@ -11,9 +11,11 @@ get_header();
 ?>
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-  <header class="mmm35-hero">
-      <?php the_post_thumbnail( 'post-thumbnail', [ 'class' => 'mmm35-hero__image' ] ); ?>
-  </header>
+  <?php if (has_post_thumbnail()) : ?>
+    <header class="mmm35-hero">
+        <?php the_post_thumbnail( 'post-thumbnail', [ 'class' => 'mmm35-hero__image' ] ); ?>
+    </header>
+  <?php endif; ?>
   <main class="mmm35-main mmm35-post">
     <?php the_content(); ?>
   </main>
