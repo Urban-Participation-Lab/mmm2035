f0deccc280d925169c1e8d0568da17a73e74aa35
Benjamin Bädorf <hello@benjaminbaedorf.eu>
2020-06-15 18:58:56 +0200

    Only animate content of post list items

--- wp-content/themes/MMM35/index.php
+++ wp-content/themes/MMM35/index.php
@@ -9,7 +9,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 get_header();
 
-$index = 0;
 ?>
 
 <div class="mmm35-page">
@@ -18,14 +17,11 @@ $index = 0;
   <main class="mmm35-main">
     <div class="mmm35-post-list">
       <div class="mmm35-post-list__items">
-      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); $index++; ?>
+      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
         <a
           href="<?php the_permalink(); ?>"
           title="<?php the_title_attribute(); ?>"
-          class="mmm35-post-list__item animate"
-          data-josh-anim-name="fadeInUpOpague"
-          data-josh-duration="400ms"
-          data-josh-delay="<?php echo $index * 50 ?>ms"
+          class="mmm35-post-list__item"
         >
           <?php if (has_post_thumbnail()) : ?>
             <figure class="mmm35-figure mmm35-post-list__item-image">
@@ -33,18 +29,25 @@ $index = 0;
               <figcaption class="mmm35-figure__caption"><?php the_post_thumbnail_caption(); ?></figcaption>
             </figure>
           <?php endif; ?>
-          <?php
-            $types = get_post_meta( $post->ID, 'type' );
-            $dates = get_post_meta( $post->ID, 'date' );
-            $meta = array();
-            if (!empty($types)) { array_push( $meta, implode( $types, ', ' ) ); }
-            if (!empty($dates)) { array_push( $meta, implode( $dates, ', ' ) ); }
-            if (!empty($meta)) :
-          ?>
-            <div class="mmm35-post-list__item-meta"><?php echo implode($meta, ', '); ?></div>
-          <?php endif; ?>
-          <h2 class="mmm35-post-list__item-title"><?php the_title(); ?></h2>
-          <p><?php echo wp_trim_words( wp_strip_all_tags( get_the_content() ), 15, '...' ); ?></p>
+          <div
+            class="mmm35-post-list__item-data animate"
+            data-josh-anim-name="fadeInUp"
+            data-josh-duration="400ms"
+            data-josh-anim-delay="400ms"
+          >
+            <?php
+              $types = get_post_meta( $post->ID, 'type' );
+              $dates = get_post_meta( $post->ID, 'date' );
+              $meta = array();
+              if (!empty($types)) { array_push( $meta, implode( $types, ', ' ) ); }
+              if (!empty($dates)) { array_push( $meta, implode( $dates, ', ' ) ); }
+              if (!empty($meta)) :
+            ?>
+              <div class="mmm35-post-list__item-meta"><?php echo implode($meta, ', '); ?></div>
+            <?php endif; ?>
+            <h2 class="mmm35-post-list__item-title"><?php the_title(); ?></h2>
+            <p><?php echo wp_trim_words( wp_strip_all_tags( get_the_content() ), 15, '...' ); ?></p>
+          </div>
         </a>
       <?php endwhile; else : ?>
         <p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
--- wp-content/themes/MMM35/template-parts/category-item-list.php
+++ wp-content/themes/MMM35/template-parts/category-item-list.php
@@ -19,14 +19,11 @@ $posts = get_posts(array( 'numberposts' => 10, 'category' => $category->cat_ID )
 <div class="mmm35-post-list">
   <h2 class="mmm35-post-list__title">Weitere <?php echo $category->name ?></h2>
   <div class="mmm35-post-list__items">
-  <?php foreach ( $posts as $index=>$post ) : ?>
+  <?php foreach ( $posts as $post ) : ?>
     <a
+      class="mmm35-post-list__item"
       href="<?php the_permalink( $post->ID ); ?>"
       title="<?php the_title_attribute($post->ID); ?>"
-      class="mmm35-post-list__item animate"
-      data-josh-anim-name="fadeInUpOpague"
-      data-josh-duration="400ms"
-      data-josh-anim-delay="<?php echo $index * 50 ?>ms"
     >
       <?php if (has_post_thumbnail( $post->ID )) : ?>
         <figure class="mmm35-figure mmm35-post-list__item-image">
@@ -34,18 +31,25 @@ $posts = get_posts(array( 'numberposts' => 10, 'category' => $category->cat_ID )
           <figcaption class="mmm35-figure__caption"><?php the_post_thumbnail_caption( $post->ID ); ?></figcaption>
         </figure>
       <?php endif; ?>
-      <?php
-        $types = get_post_meta( $post->ID, 'type' );
-        $dates = get_post_meta( $post->ID, 'date' );
-        $meta = array();
-        if (!empty($types)) { array_push( $meta, implode( $types, ', ' ) ); }
-        if (!empty($dates)) { array_push( $meta, implode( $dates, ', ' ) ); }
-        if (!empty($meta)) :
-      ?>
-        <div class="mmm35-post-list__item-meta"><?php echo implode($meta, ', '); ?></div>
-      <?php endif; ?>
-      <h2 class="mmm35-post-list__item-title"><?php echo get_the_title( $post ); ?></h2>
-      <p><?php echo wp_trim_words( wp_strip_all_tags( get_the_content( $post->ID ) ), 15, '...' ); ?></p>
+      <div
+        class="mmm35-post-list__item-data animate"
+        data-josh-anim-name="fadeInUp"
+        data-josh-duration="400ms"
+        data-josh-anim-delay="400ms"
+      >
+        <?php
+          $types = get_post_meta( $post->ID, 'type' );
+          $dates = get_post_meta( $post->ID, 'date' );
+          $meta = array();
+          if (!empty($types)) { array_push( $meta, implode( $types, ', ' ) ); }
+          if (!empty($dates)) { array_push( $meta, implode( $dates, ', ' ) ); }
+          if (!empty($meta)) :
+        ?>
+          <div class="mmm35-post-list__item-meta"><?php echo implode($meta, ', '); ?></div>
+        <?php endif; ?>
+        <h2 class="mmm35-post-list__item-title"><?php echo get_the_title( $post ); ?></h2>
+        <p><?php echo wp_trim_words( wp_strip_all_tags( get_the_content( $post->ID ) ), 15, '...' ); ?></p>
+      </div>
     </a>
   <?php endforeach; ?>
   </div>
