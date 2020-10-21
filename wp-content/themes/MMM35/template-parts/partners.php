4a0b23c9055d76abeb8ccc99e962b6df2005273c
Benjamin Bädorf <hello@benjaminbaedorf.eu>
2020-10-21 11:40:52 +0200

    Updated titles, partners

--- wp-content/themes/MMM35/template-parts/partners.php
+++ wp-content/themes/MMM35/template-parts/partners.php
@@ -9,34 +9,20 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 ?>
 <div class="mmm35-partners">
-  <h2>Mitwirkende</h2>
+  <h2>Realisation</h2>
   <div class="mmm35-partners__list">
     <a
       rel="norefferer noopener"
-      href="https://www.orangeedge.de/"
+      href="https://www.muenster.de/"
       target="_blank"
     >
       <img
-        src="<?php echo get_template_directory_uri(); ?>/assets/partners/orange-edge.png"
-        alt="Logo von Orange Edge"
+        src="<?php echo get_template_directory_uri(); ?>/assets/partners/muenster.png"
+        alt="Logo von der Stadt Münster"
         class="animate"
         data-josh-anim-name="fadeInUp"
         data-josh-duration="400ms"
         data-josh-anim-delay="500ms"
-      />
-    </a>
-    <a
-      rel="norefferer noopener"
-      href="https://www.ssp-consult.de/"
-      target="_blank"
-    >
-      <img
-        src="<?php echo get_template_directory_uri(); ?>/assets/partners/ssp.png"
-        alt="Logo von SSP"
-        class="animate"
-        data-josh-anim-name="fadeInUp"
-        data-josh-duration="400ms"
-        data-josh-anim-delay="600ms"
       />
     </a>
     <a
